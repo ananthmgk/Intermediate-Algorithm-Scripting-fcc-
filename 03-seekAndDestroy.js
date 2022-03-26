ea1fa67ce6a9e98b2da607738a7c7d2974a8027f
ananthmgk <83916278+ananthmgk@users.noreply.github.com>
2022-03-26 23:17:20 +0530

    comments added

--- 03-seekAndDestroy.js
+++ 03-seekAndDestroy.js
@@ -1,7 +1,9 @@
+// Aim:  Remove all elements from the initial array that are of the same value as these arguments.
 function destroyer(arr,...args) {
   const newArr = [];
   arr.map(a => (args.indexOf(a) == -1 && newArr.push(a)));
   return newArr;
 }
 
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); 
+// => [1, 1]
