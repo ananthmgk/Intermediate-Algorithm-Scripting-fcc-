2b56f76d94808c4027639147855f1542f3ae1221
ananthmgk <83916278+ananthmgk@users.noreply.github.com>
2022-03-27 00:06:09 +0530

    comments added

--- 04-whereforeArtThou.js
+++ 04-whereforeArtThou.js
@@ -1,29 +1,35 @@
-Method :1
+// Aim : Make a function that looks through an array of objects (first argument) 
+//       and returns an array of all objects that have matching name and value pairs (second argument). 
+// Method :1
 
+// extract the keys in the source 
 function intoArr(obj) {
   let newArr = [];
   for(let prop in obj) {
     newArr.push(prop);
-  } return newArr;
+  } return newArr; // => it will return [ 'last' ].
 };
 
 function whatIsInAName(collection, source) {
   const arr = [];
   for(let i = 0; i < collection.length; i++) {
+    // by using every func, checks every 'last' keys is matching in collection.
       if(intoArr(source).every(a => collection[i][a] === source[a])) {
         arr.push(collection[i]);
       };
   };
   return arr;
-};
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
-------------------*-----------------------*----------------------------------*-----------------------------*----------------------------
-Method :2
 
-Using Object.keys();
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+// it will return => [ { first: 'Tybalt', last: 'Capulet' } ]..
+// ------------------*-----------------------*----------------------------------*-----------------------------*----------------------------
+// Method :2
+
+// Using Object.keys();
 function whatIsInAName(collection, source) {
   const arr = [];
   for(let i = 0; i < collection.length; i++) {
+    // here object.keys will extract the keys in source. and checks every matching keys.
       if(Object.keys(source).every(a => collection[i][a] === source[a])) {
         arr.push(collection[i]);
       };
@@ -31,4 +37,5 @@ function whatIsInAName(collection, source) {
   return arr;
 };
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
---------------*------------------------*--------------------------------------*------------------------------------*----------------------
+// it will return => [ { first: 'Tybalt', last: 'Capulet' } ]..
+// --------------*------------------------*--------------------------------------*------------------------------------*----------------------
