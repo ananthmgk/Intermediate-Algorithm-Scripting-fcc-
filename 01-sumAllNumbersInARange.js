5b8bbd3e34b9bbb623ff98072a56d4dc4d0f0a5d
ananthmgk <83916278+ananthmgk@users.noreply.github.com>
2022-03-26 12:25:59 +0530

    comments are added

--- 01-sumAllNumbersInARange.js
+++ 01-sumAllNumbersInARange.js
@@ -1,24 +1,28 @@
-Method 1:
+// Method 1:
 
 function sumAll(arr) {
-  const sortedArr = arr.sort((a,b) => b-a)
+  const sortedArr = arr.sort((a,b) => b-a);
+  // sorted the given arguments
   const newArr = [];
   for(let i = sortedArr[0]; i >= sortedArr[1]; i--) {
-    newArr.push(i);
+    newArr.push(i); // here the newArr will collect [ 10, 9, 8, 7, 6, 5 ]
   }; 
+  // so by using reduce() it will sum the returned newArr
   return newArr.reduce((initial,currentElement) => initial+currentElement,0);
 };
-console.log(sumAll([10, 5]));
---------*--------------*--------------------------*------------------
-Method 2:
-without reduce function
+console.log(sumAll([10, 5])); //=>45
+// --------*--------------*--------------------------*------------------*-------------------------------------
+// Method 2:
+// without reduce function
 
 function sumAll(arr) {
-  const sortedArr = arr.sort((a,b) => b-a)
+  const sortedArr = arr.sort((a,b) => b-a);
+  // sorted the given arguments
   let total = 0;
+  // by extracting and sum the numbers between 5 to 10
   for(let i = sortedArr[0]; i >= sortedArr[1]; i--) {
     total += i;
   }; 
   return total;
 };
-console.log(sumAll([5, 10]));
+console.log(sumAll([5, 10])); //=>45
